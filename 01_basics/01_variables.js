8ea91590ccbe8142e68fa7a6b6eccf3b160393ec
Lucifer-002 <70974246+Lucifer-002@users.noreply.github.com>
2024-03-05 18:16:39 +0530

    Add files via upload

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const account_id = 1445368
+var email = "chirantanmazumdar@gmail.com"
+let account_password = "12345"
+
+accountCity = "kolkata"
+// account_id = 2 // Not aloowed cant change a variable declared const 
+console.table([account_id,email,account_password,accountCity])
+
+email = "i_am_tamal@gmail.com"
+account_password="Amit69"
+accountCity="Bengaluru"
+console.table([account_id,email,account_password,accountCity])
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict" // All codes will be considered as newer version of JS
+
+let name = "Chiru"
+let age = 21 
+let IsloggedIn = false 
+
+// number 
+// Boolean
+// bigint 
+// string 
+// null 
+// undefined
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof IsloggedIn);
+console.log(typeof null);
+console.log(typeof undefined);
