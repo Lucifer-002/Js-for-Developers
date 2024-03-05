261591c17564250f79f3c902d3b287c21aa9cc79
Lucifer-002 <70974246+Lucifer-002@users.noreply.github.com>
2024-03-05 18:43:16 +0530

    Add files via upload

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,41 @@
+// let score = "33" 
+
+// console.log(typeof(score)); // String 
+// console.log(typeof score); // String
+
+// let updatedScore = Number(score)
+// console.log(typeof(updatedScore)); // Number 
+// console.log(updatedScore); // Number 
+
+// let score = "33abc" 
+// console.log(typeof(score)); // String 
+
+// let updatedScore = Number(score)
+// console.log(typeof(updatedScore)); // Number 
+// console.log(updatedScore); // NaN
+
+
+// let IsloggedIn = 0 
+// console.log(typeof IsloggedIn);
+
+// let newlog = Boolean(IsloggedIn)
+// console.table([typeof(newlog) , newlog])
+
+// 1 ==> True  0 ==> false
+
+// let IsloggedIn = ""
+// let str = "hitesh"
+// console.table([typeof(IsloggedIn) , typeof(str)]);
+
+// let newlog = Boolean(IsloggedIn)
+// let newstr = Boolean(str)
+// console.table([typeof(newlog) , newlog , typeof(newstr) , newstr])
+
+let score = 33
+
+console.log(typeof(score)); // String 
+
+let updatedScore = String(score)
+console.log(typeof(updatedScore)); 
+console.log(updatedScore); 
+
